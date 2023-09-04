67a0463f06050c9cebcf77b0204afcb8665e37b8
MauR27 <antonio27mauricio27@gmail.com>
2023-09-04 18:41:58 +0200

    added cors

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import userRoutes from "./routes/userRoutes.js";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 dotenv.config();
 const port = process.env.PORT || 5000;
 
@@ -15,6 +17,7 @@ app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/api/users", userRoutes);
 
