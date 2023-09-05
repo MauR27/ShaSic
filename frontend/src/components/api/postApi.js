a1edd9d74fb9a9bbefaf8f6d75830444bf772b36
MauR27 <antonio27mauricio27@gmail.com>
2023-09-05 16:34:38 +0200

    .

--- frontend/src/components/api/postApi.js
+++ frontend/src/components/api/postApi.js
@@ -1,10 +1,7 @@
 const URL_API = process.env.REACT_APP_SERVER_URL;
 
 export const fetchApiPost = async () => {
-  const response = await fetch(`${URL_API}/post`, {
-    credentials: "include",
-    mode: "cors",
-  });
+  const response = await fetch(`${URL_API}/post`);
   const json = await response.json();
   return json.post.map((data) => {
     return data;
--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: process.env.REACT_APP_SERVER_URL });
+const baseQuery = fetchBaseQuery({
+  baseUrl: process.env.REACT_APP_SERVER_URL,
+  credentials: "include",
+});
 
 export const apiSlice = createApi({
   baseQuery,
