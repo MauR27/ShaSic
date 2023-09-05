529738510385a9310c9ac083ab553f41776b4e9c
MauR27 <antonio27mauricio27@gmail.com>
2023-09-05 14:08:15 +0200

    .

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -6,13 +6,13 @@ const protect = asyncHandler(async (req, res, next) => {
   let token;
 
   token = req.cookies.jwt;
-
+  console.log(token);
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+      console.log(decoded);
       req.user = await User.findById(decoded.userId).select("-password");
-
       next();
     } catch (error) {
       res.status(401);
