2a8eb0195d34d5e7dfcafbc0558f7e5e1f3e37de
MauR27 <antonio27mauricio27@gmail.com>
2023-09-05 17:07:15 +0200

    .

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: process.env.REACT_APP_SERVER_URL,
-  // credentials: "include",
+  credentials: "include",
 });
 
 export const apiSlice = createApi({
