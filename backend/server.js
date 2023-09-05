3f9dd25b87a74e169d97f00a320e4ea1f2a51308
MauR27 <antonio27mauricio27@gmail.com>
2023-09-05 16:46:03 +0200

    set credential tu cors

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ credentials: true }));
 
 app.use("/api/users", userRoutes);
 
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,6 +7,8 @@ const generateToken = (res, userId) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "None",
     maxAge: 10 * 24 * 60 * 60 * 1000,
   });
 };
