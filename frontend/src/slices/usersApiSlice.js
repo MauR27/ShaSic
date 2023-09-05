066e7bc2fa2fd80d8a556e5d815d03d3dfee484f
MauR27 <antonio27mauricio27@gmail.com>
2023-09-05 10:20:07 +0200

    fix typo and changing in redux mutations the url

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,53 +1,60 @@
 import { apiSlice } from "./apiSlice";
 
-const USERS_URL = "/api/users";
+// const USERS_URL = '/api/users'
 
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (data) => ({
-        url: `${USERS_URL}/auth`,
+        // url: `${USERS_URL}/auth`,
+        url: `/auth`,
         method: "POST",
         body: data,
       }),
     }),
     register: builder.mutation({
       query: (data) => ({
-        url: `${USERS_URL}`,
+        // url: `${USERS_URL}`,
+        url: "",
         method: "POST",
         body: data,
       }),
     }),
     logout: builder.mutation({
       query: () => ({
-        url: `${USERS_URL}/logout`,
+        // url: `${USERS_URL}/logout`,
+        url: `/logout`,
         method: "POST",
       }),
     }),
     updateUser: builder.mutation({
       query: (data) => ({
-        url: `${USERS_URL}/profile`,
+        // url: `${USERS_URL}/profile`,
+        url: `/profile`,
         method: "PUT",
         body: data,
       }),
     }),
     postData: builder.mutation({
       query: (data) => ({
-        url: `${USERS_URL}/post`,
+        // url: `${USERS_URL}/post`,
+        url: `/post`,
         method: "POST",
         body: data,
       }),
     }),
     getPostData: builder.mutation({
       query: (data) => ({
-        url: `${USERS_URL}/post`,
+        // url: `${USERS_URL}/post`,
+        url: `/post`,
         method: "GET",
         body: data,
       }),
     }),
     getUsersData: builder.mutation({
       query: (data) => ({
-        url: `${USERS_URL}/auth`,
+        // url: `${USERS_URL}/auth`,
+        url: `/auth`,
         method: "GET",
         body: data,
       }),
@@ -55,7 +62,8 @@ export const usersApiSlice = apiSlice.injectEndpoints({
     addComment: builder.mutation({
       query: (data) => {
         return {
-          url: `${USERS_URL}/post/${data.values.postId}/comments`,
+          // url: `${USERS_URL}/post/${data.values.postId}/comments`,
+          url: `/post/${data.values.postId}/comments`,
           method: "POST",
           body: data.values,
         };
