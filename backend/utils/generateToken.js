b2374a96f51444e4e4a343523182de587844de21
MauR27 <antonio27mauricio27@gmail.com>
2023-09-05 12:09:24 +0200

    change the httpOnly to false and secure to false

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,8 +6,9 @@ const generateToken = (res, userId) => {
   });
 
   res.cookie("jwt", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
+    httpOnly: false,
+    // secure: process.env.NODE_ENV !== "development",
+    secure: false,
     sameSite: "strict",
     maxAge: 10 * 24 * 60 * 60 * 1000,
   });
