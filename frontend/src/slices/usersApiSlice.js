7fc427e48a6701099f6d5ac93781da375c3cd4ec
MauR27 <antonio27mauricio27@gmail.com>
2023-09-05 11:30:30 +0200

    typo fix

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,35 +1,35 @@
 import { apiSlice } from "./apiSlice";
 
-// const USERS_URL = '/api/users'
+const USERS_URL = "/api/users";
 
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (data) => ({
-        // url: `${USERS_URL}/auth`,
-        url: `/auth`,
+        url: `${USERS_URL}/auth`,
+        // url: `/auth`,
         method: "POST",
         body: data,
       }),
     }),
     register: builder.mutation({
       query: (data) => ({
-        // url: `${USERS_URL}`,
-        url: "",
+        url: `${USERS_URL}`,
+        // url: '',
         method: "POST",
         body: data,
       }),
     }),
     logout: builder.mutation({
       query: () => ({
-        // url: `${USERS_URL}/logout`,
-        url: `/logout`,
+        url: `${USERS_URL}/logout`,
+        // url: `/logout`,
         method: "POST",
       }),
     }),
     updateUser: builder.mutation({
       query: (data) => ({
-        // url: `${USERS_URL}/profile`,
+        url: `${USERS_URL}/profile`,
         url: `/profile`,
         method: "PUT",
         body: data,
@@ -37,24 +37,24 @@ export const usersApiSlice = apiSlice.injectEndpoints({
     }),
     postData: builder.mutation({
       query: (data) => ({
-        // url: `${USERS_URL}/post`,
-        url: `/post`,
+        url: `${USERS_URL}/post`,
+        // url: `/post`,
         method: "POST",
         body: data,
       }),
     }),
     getPostData: builder.mutation({
       query: (data) => ({
-        // url: `${USERS_URL}/post`,
-        url: `/post`,
+        url: `${USERS_URL}/post`,
+        // url: `/post`,
         method: "GET",
         body: data,
       }),
     }),
     getUsersData: builder.mutation({
       query: (data) => ({
-        // url: `${USERS_URL}/auth`,
-        url: `/auth`,
+        url: `${USERS_URL}/auth`,
+        // url: `/auth`,
         method: "GET",
         body: data,
       }),
@@ -62,8 +62,8 @@ export const usersApiSlice = apiSlice.injectEndpoints({
     addComment: builder.mutation({
       query: (data) => {
         return {
-          // url: `${USERS_URL}/post/${data.values.postId}/comments`,
-          url: `/post/${data.values.postId}/comments`,
+          url: `${USERS_URL}/post/${data.values.postId}/comments`,
+          // url: `/post/${data.values.postId}/comments`,
           method: "POST",
           body: data.values,
         };
