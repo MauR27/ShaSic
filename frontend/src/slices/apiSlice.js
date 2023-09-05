7d0f0c0aae6730c7c3619de37d6dcc30af7e1cea
MauR27 <antonio27mauricio27@gmail.com>
2023-09-05 09:58:41 +0200

    fixed typo and added a url to the fetchbasequery redux

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "" });
+const baseQuery = fetchBaseQuery({ baseUrl: process.env.REACT_APP_SERVER_URL });
 
 export const apiSlice = createApi({
   baseQuery,
--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,6 +1,6 @@
 import { apiSlice } from "./apiSlice";
 
-const USERS_URL = process.env.REACT_APP_SERVER_URL;
+const USERS_URL = "/api/users";
 
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
